ad0ed739a93efc7e3444bf7357c93e1287f0d347
Sai.Hein <81770760+R-World007@users.noreply.github.com>
2023-01-27 15:37:48 +0630

    Update list.py

--- list.py
+++ list.py
@@ -6,3 +6,19 @@
 # ordered, changeable, and duplicate values
 mylist = ["apple","orange","cherry"]
 print(mylist)
+# List length
+print(len(mylist))
+
+# list items
+list1= ["Sai","Moe","Hein","Hein"] # str, allow duplicate
+list2= [1,2,3] # int
+list3= [True,False] # boolean
+list4= ["Sai",16,True] # allinone
+print(list1,list2,list3,list4)
+
+# list type
+print(type(list1))
+
+# list() constructor
+list5= list(("apple",4))
+print(list5)
