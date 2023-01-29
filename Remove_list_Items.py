b6283fac94d45b1e5f5a0508f2b1111b0f02b743
Sai.Hein <81770760+R-World007@users.noreply.github.com>
2023-01-29 23:51:38 +0630

    Add files via upload

---
+++ Remove_list_Items.py
@@ -0,0 +1,31 @@
+#remove() Specified item
+list1=['Apple','Banana','Cherry']
+list1.remove('Banana')
+print(list1)
+
+#pop() Remove Specified Index
+list2= ['hi','hola','Hey']
+list2.pop(1)
+print(list2)
+list2= ['hi','hola','Hey']
+list2.pop() # remove the last item
+print(list2)
+
+# DEL
+list3 = ['apple','cherry','banana']
+del list3[0]
+print(list3)
+
+list4 = ['hi','hey']
+del list4 # del the entire list
+
+# clear()
+list5 = ['cherry','banana','apple']
+list5.clear()
+print(list5)
+
+
+
+
+
+
---
+++ add list items.py
@@ -0,0 +1,20 @@
+
+# append()
+list1 = ["Hi","Hello","Hey"]
+list1.append("Hola")
+print(list1)
+
+# insert() items
+list2=["Hola","Hi","hey"]
+list2.insert(1,"Hello")
+print(list2)
+
+# Add any lterable #extend()
+list3=['apple','orange','kwi']
+list4_tuple = ("pineapple","Orange")
+list3.extend(list4_tuple)
+print(list3)
+
+
+
+
