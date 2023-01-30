98118697f8cbb9e03eb09b9a3d29a7017880759c
Sai.Hein <81770760+R-World007@users.noreply.github.com>
2023-01-30 22:19:09 +0630

    Update list_compersion.py

--- list_compersion.py
+++ list_compersion.py
@@ -25,3 +25,28 @@ print (list3)
 # only accept only numbers lower than 5
 list4 = [n for n in range(10) if n < 5]
 print(list4)
+
+# new lists to upper case
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x.upper() for x in fruits]
+
+print(newlist)
+
+# new list to Hello
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = ['hello' for x in fruits]
+
+print(newlist)
+
+# orange to banana
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x if x != "banana" else "orange" for x in fruits]
+
+print(newlist)
+
+
+
+
